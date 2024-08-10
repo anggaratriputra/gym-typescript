f9c6896f4c3281afc9753e0033742c6bcf9ae4e0
anggaratriputra <anggaratriputra@gmail.com>
2024-08-10 11:49:32 +0700

    new changes on tsconfig

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,5 +1,4 @@
 import { SelectedPage } from "@/shared/types";
-import React from "react";
 import { useForm } from "react-hook-form";
 import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
 import { motion } from "framer-motion";
